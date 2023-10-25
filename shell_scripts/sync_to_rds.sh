50341737a7081cfb2625e5294f1e82f5649e608f
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2023-10-25 11:54:26 -0700

    Delete shell_scripts directory
    name was changed to 'scripts' in local mesabi repo, so just updating to make them the same

--- shell_scripts/sync_to_rds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=2:00:00
-#SBATCH --mem-per-cpu=10g
-#SBATCH --tmp=10g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=shibainu372@gmail.com
-#SBATCH -o rscript_exit.out
-module load R
-Rscript create_paired_fsts.R
