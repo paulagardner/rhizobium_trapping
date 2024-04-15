4c553c29796d0ce92478382828f69da690a60b68
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2024-04-15 09:23:31 -0700

    Update make_bam_list.sh

--- scripts/make_bam_list.sh
+++ scripts/make_bam_list.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-#SBATCH --time = 2:00:00
-#SBATCH --ntasks = 1
-#SBATCH --mem = 10g
-#SBATCH --tmp = 10g
-#SBATCH --mail-type = ALL
-#SBATCH --mail-user = shibainu372@gmail.com
 
-#cd ~/Fudge016/TrappingCh2/bamfiles 
+cd ~/Fudge016/TrappingCh2/bamfiles #change shells to where the sorted bam files are
 if [ -s $~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt ]; then #this says: if the contents of the file in question are empty, then do the for() loop
 	for file in ~/Fudge016/TrappingCh2/bamfiles/*sorted; do
-		echo "$(pwd)/$file" >> ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt;
+		echo "$(pwd)/$file" >> ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt; #this path is where the .txt is going to live. alter as necessary. 
 	done #done is needed whenever there's a for() loop. shellcheck.net is SUPER helpful here
 else 
 	echo "this file already has a list of .bams! it may already contain what you need- it has" $( wc -l ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt | cut -f1 -d' ') "lines";
