709bda1899b527355922e4c3e553652f0a382a26
Paula G <paula.dorothyg@gmail.com>
2024-05-01 15:45:17 -0500

    check on github

--- scripts/Rmodeling.R
+++ scripts/Rmodeling.R
@@ -1,8 +1,10 @@
 #!/usr/bin/env Rscript
 # Load the model from file
 library("tidyverse")
+library("poolfstat")
 
-matrix_rds <- read_rds('~/Fudge016/TrappingCh2/rhizobium_trapping/data/pairwisefsts.rds')
+
+matrix_rds <- read_rds('pairwisefsts.rds')
 print("matrix loaded, checking")
 ls()
 # Use the model however you want
