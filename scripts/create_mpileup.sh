4f4130a9c6fa7d03694499af021a0e0ac13e36d7
Paula G <paula.dorothyg@gmail.com>
2024-06-26 08:28:42 -0500

    sending mpileup to be able to paste

--- scripts/create_mpileup.sh
+++ scripts/create_mpileup.sh
@@ -5,6 +5,6 @@
 #SBATCH --tmp=10g
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shibainu372@gmail.com
-module load samtools/1.10
-#the long file in paula_dir is the reference genome, against which samtools will compare the sorted .bam files. 
-samtools mpileup -b ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt -f ~/Fudge016/TrappingCh2/paula_dir/refseq_reference_coded_with_N/ncbi_dataset/data/GCF_000009265.1/GCF_000009265.1_ASM926v1_genomic.fna -o ~/Fudge016/TrappingCh2/rhizobium_trapping/data/all_bams.mpileup
+module load samtools/1.16.1-gcc-8.2.0-egljrr3
+#the long file in paula_dir is the reference genome, against which samtools will compare the sorted .bam files. i
+samtools mpileup -b ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt -f   -o ~/Fudge016/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup
