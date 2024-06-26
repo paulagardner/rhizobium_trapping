89a89495ce7fc639bc0ddf47a546d3378a3b586d
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2024-06-26 06:33:10 -0700

    Update create_mpileup.sh with line continuation

--- scripts/create_mpileup.sh
+++ scripts/create_mpileup.sh
@@ -7,5 +7,6 @@
 #SBATCH --mail-user=shibainu372@gmail.com
 module load samtools/1.16.1-gcc-8.2.0-egljrr3
 #the long file in paula_dir is the reference genome, against which samtools will compare the sorted .bam files. i
-samtools mpileup -b ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt -f ~/Fudge016/TrappingCh2/rhizobium_trapping/data/refseq_reference_coded_with_N/ncbi_dataset/data/GCF_0000092
-65.1/GCF_000009265.1_ASM926v1_genomic.fna -o ~/Fudge016/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup
+samtools mpileup -b ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt \
+-f ~/Fudge016/TrappingCh2/rhizobium_trapping/data/refseq_reference_coded_with_N/ncbi_dataset/data/GCF_000009265.1/GCF_000009265.1_ASM926v1_genomic.fna \
+-o ~/Fudge016/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup
