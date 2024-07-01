d22a54706364037f5d1854bcbe25b134985f9648
Paula G <paula.dorothyg@gmail.com>
2024-07-01 17:05:40 -0500

    ensuring .sync works, make it send output to data folder

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,8 +6,17 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shibainu372@gmail.com
 
+#two ways of approaching it. The faster way:
 #popoolation2_path = ~/Fudge016/Software/PoPoolation/popoolation2_1201/
 java -ea -Xmx7g -jar ~/Fudge016/Software/PoPoolation/popoolation2_1201/mpileup2sync.jar \
 	--input ~/Fudge016/TrappingCh2/rhizobium_trapping/data/all_bams.mpileup \
 	--output ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sync_all_bams.sync \
 	--fastq-type sanger --min-qual 20 --threads 8
+
+#perl ~/Fudge016/Software/PoPoolation/popoolation2_1201/mpileup2sync.pl \
+#	--fastq-type sanger \
+#	--min-qual 20 \
+#	--input ~/Fudge016/TrappingCh2/rhizobium_trapping/data/all_bams.mpileup \
+#	--output ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sync_all_bams.sync
+
+
