aa0e3694a6301d72df42c250a91b7f0ab7b02904
paulagardner <paula@resuscitatedpc>
2024-08-08 00:36:33 -0700

    get sort.sh and make_bamfiles_list working on paula machine

--- scripts/make_bam_list.sh
+++ scripts/make_bam_list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< Updated upstream
 cd ~/Fudge016/TrappingCh2/rhizobium_trapping/data/bamfiles #change shells to where the sorted bam files are
 
 if [ ! -f $~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt ]; then
@@ -17,6 +18,23 @@ if [ ! -s $~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt ]
 	echo "list populated with" $( wc -l ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt | cut -f1 -d' ') "lines"
 else 
 	echo "file is not empty! It may already contain what you need. it has" $( wc -l ~/Fudge016/TrappingCh2/rhizobium_trapping/data/sorted_bam_files.txt | cut -f1 -d' ') "lines, implying that many BAM files."
+=======
+cd ~/rhizobium_trapping/data/bamfiles || exit  #change shells to where the sorted bam files are
+
+if [ ! -f $~/rhizobium_trapping/data/sorted_bam_files.txt ]; then
+        echo "file does not exist, making file"
+        touch ~/rhizobium_trapping/data/sorted_bam_files.txt
+        echo "file made"
+fi
+
+if [ ! -s $~/rhizobium_trapping/data/sorted_bam_files.txt ]; then #this says: if the contents of the file in question are empty, then do the for() loop
+        for file in ~/rhizobium_trapping/data/bamfiles/*sorted; do
+                echo "$(pwd)/$file" >> ~/rhizobium_trapping/data/sorted_bam_files.txt; #this path is where the .txt is going to live. alter as necessary. 
+        done #done is needed whenever there's a for() loop. shellcheck.net is SUPER helpful here 
+        echo "list populated with " $( wc -l ~/rhizobium_trapping/data/sorted_bam_files.txt | cut -f1 -d' ') "lines"
+else
+        echo "file is not empty! It may already contain what you need. it has" $( wc -l ~/rhizobium_trapping/data/sorted_bam_files.txt | cut -f1 -d' ') "lines, implying that many BAM files."
+>>>>>>> Stashed changes
 
 fi #fi is needed to complete a if() statement
 
