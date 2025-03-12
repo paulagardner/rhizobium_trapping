6a9e0506b2fa7055345b269afd391a81cc45963d
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2025-03-12 08:19:29 +0000

    re-running some analyses; code has changed due to using new computing resources

--- scripts/allelefreqs.sh
+++ scripts/allelefreqs.sh
@@ -2,7 +2,7 @@
 
 #running into some issues: https://sourceforge.net/p/popoolation2/tickets/32/?page=1
 
-perl ~/popoolation2_1201/snp-frequency-diff.pl \
+perl ~/software/popoolation2_1201/snp-frequency-diff.pl \
 	--input ~/rhizobium_trapping/data/sync_all_bams.sync \
 	--output-prefix ~/rhizobium_trapping/data/samtools_1_10_test \
 	--max-coverage 200
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,15 +2,13 @@
 
 #two ways of approaching it. The faster way:
 #popoolation2_path = ~/popoolation2_1201
-#java -ea -Xmx7g -jar ~/popoolation2_1201/mpileup2sync.jar \
-#	--input ~/rhizobium_trapping/data/documentation-test.mpileup \
-#	--output ~/rhizobium_trapping/data/sync_all_bams.sync \
+#java -ea -Xmx7g -jar ~/software/popoolation2_1201/mpileup2sync.jar \
+#	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
+#	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync \
 #	--fastq-type sanger --min-qual 20 --threads 8
-
-perl ~/popoolation2_1201/mpileup2sync.pl \
+perl ~/software/popoolation2_1201/mpileup2sync.pl \
 	--fastq-type sanger \
 	--min-qual 20 \
-	--input ~/rhizobium_trapping/data/documentation-test.mpileup \
-	--output ~/rhizobium_trapping/data/sync_all_bams.sync
-
+	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
+	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync
 
