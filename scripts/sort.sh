55ab467f9e58942cfffdd7976a94eb8c8a313d1a
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2025-04-04 15:59:06 +0100

    Update sort.sh

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,24 +1,16 @@
-#!/usr/bin/bash 
-#SBATCH --time=6:00:00
-#SBATCH --ntasks=1
-#SBATCH --mem=10g
-#SBATCH --tmp=10g
+#!/bin/sh
 
-<<<<<<< Updated upstream
+#two ways of approaching it. The faster way:
+#popoolation2_path = ~/popoolation2_1201
+java -ea -Xmx7g -jar ~/software/popoolation2_1201/mpileup2sync.jar \
+	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
+	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync \
+	--fastq-type sanger --min-qual 20 --threads 8
+	
+#perl ~/software/popoolation2_1201/snp-frequency-diff.pl \
+#	--fastq-type sanger \
+#	--min-qual 20 \
+#	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
+#	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync
 
-cd ~/Fudge016/TrappingCh2/rhizobium_trapping/data/bamfiles
-#this is for the HPC we're working with- using an old version of samtools (1.10) gave a 
-# 'libcrypto.so.10' error 
-module load samtools/1.16.1-gcc-8.2.0-egljrr3
-=======
-cd ~/rhizobium_trapping/data/bamfiles
-module load samtools/1.10
->>>>>>> Stashed changes
 
-#this should not generate extraneous files, as whenever <something>.sorted is generated, 
-#it will overwrite the previous instance of <something>.sorted
-#however, this script doesn't fare well with leftover .tmp files from 
-#unfinished sort processes. be sure to remove if this is the case.
-for f in *.bam; 
-	do  samtools sort -o ${f/bam/sorted} $f #https://www.biostars.org/p/9537028/
-done
