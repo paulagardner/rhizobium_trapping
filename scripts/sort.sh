ed34b2b4e27ab63089f5cc6603204ee3a2cf6eb7
Paula D. Gardner <72171469+paulagardner@users.noreply.github.com>
2025-04-04 16:30:40 +0100

    Update sort.sh

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/usr/bin/bash 
+#SBATCH --time=6:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=10g
+#SBATCH --tmp=10g
 
-#two ways of approaching it. The faster way:
-#popoolation2_path = ~/popoolation2_1201
-java -ea -Xmx7g -jar ~/software/popoolation2_1201/mpileup2sync.jar \
-	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
-	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync \
-	--fastq-type sanger --min-qual 20 --threads 8
-	
-#perl ~/software/popoolation2_1201/snp-frequency-diff.pl \
-#	--fastq-type sanger \
-#	--min-qual 20 \
-#	--input /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/documentation-test.mpileup \
-#	--output /mnt/c/Users/Paula/Desktop/TrappingCh2/rhizobium_trapping/data/old_documentation-test_revisions.sync
+cd ~/rhizobium_trapping/data/bamfiles
+module load samtools/1.10
 
-
+#this should not generate extraneous files, as whenever <something>.sorted is generated, 
+#it will overwrite the previous instance of <something>.sorted
+#however, this script doesn't fare well with leftover .tmp files from 
+#unfinished sort processes. be sure to remove if this is the case.
+for f in *.bam; 
+	do  samtools sort -o ${f/bam/sorted} $f #https://www.biostars.org/p/9537028/
+done
